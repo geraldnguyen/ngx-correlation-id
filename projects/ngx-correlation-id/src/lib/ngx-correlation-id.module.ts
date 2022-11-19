3edc967bc882e9f20610fea418cf9a2e0eea289d
Gerald Nguyen <ngthehuy@gmail.com>
2022-11-19 10:55:59 +0800

    remove empty lines

--- projects/ngx-correlation-id/src/lib/ngx-correlation-id.module.ts
+++ projects/ngx-correlation-id/src/lib/ngx-correlation-id.module.ts
@@ -2,9 +2,6 @@ import { NgModule } from '@angular/core';
 import { HTTP_INTERCEPTORS } from '@angular/common/http';
 import { CorrelationIdInterceptor } from './correlation-id.interceptor';
 
-
-
-
 @NgModule({
   declarations: [
   ],
