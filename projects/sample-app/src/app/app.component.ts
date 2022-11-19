c53f15bd3dfde2d91ac11bf005a73028b8995a36
Gerald Nguyen <ngthehuy@gmail.com>
2022-11-19 11:19:54 +0800

    sample codes

--- projects/sample-app/src/app/app.component.ts
+++ projects/sample-app/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'sample-app';
+
+  constructor(http: HttpClient) {
+    http.get('https://www.google.com').subscribe();
+  }
 }
--- projects/sample-app/src/app/app.module.ts
+++ projects/sample-app/src/app/app.module.ts
@@ -1,14 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
+import { NgxCorrelationIdModule } from 'ngx-correlation-id';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    NgxCorrelationIdModule
   ],
   providers: [],
   bootstrap: [AppComponent]
