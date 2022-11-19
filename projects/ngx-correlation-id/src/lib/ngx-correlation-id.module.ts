f9abb4e52746a97dffd7b5ceafbc9de4d0bcc634
Gerald Nguyen <ngthehuy@gmail.com>
2022-11-19 10:43:31 +0800

    drop component and service

--- projects/ngx-correlation-id/src/lib/ngx-correlation-id.module.ts
+++ projects/ngx-correlation-id/src/lib/ngx-correlation-id.module.ts
@@ -1,16 +1,13 @@
 import { NgModule } from '@angular/core';
-import { NgxCorrelationIdComponent } from './ngx-correlation-id.component';
 
 
 
 @NgModule({
   declarations: [
-    NgxCorrelationIdComponent
   ],
   imports: [
   ],
   exports: [
-    NgxCorrelationIdComponent
   ]
 })
 export class NgxCorrelationIdModule { }
--- projects/ngx-correlation-id/src/public-api.ts
+++ projects/ngx-correlation-id/src/public-api.ts
@@ -2,6 +2,4 @@
  * Public API Surface of ngx-correlation-id
  */
 
-export * from './lib/ngx-correlation-id.service';
-export * from './lib/ngx-correlation-id.component';
 export * from './lib/ngx-correlation-id.module';
